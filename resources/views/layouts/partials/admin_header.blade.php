85f62f4b6c8794cf3802d751250c40bcf9e8ec24
unknown <naimcse56@gmail.com>
2025-11-20 12:17:04 +0600

    basic setup user

--- resources/views/layouts/partials/admin_header.blade.php
+++ resources/views/layouts/partials/admin_header.blade.php
@@ -3,17 +3,7 @@
     <div class="mobile-toggle-icon d-xl-none">
         <i class="bi bi-list"></i>
     </div>
-
-    <div class="search-toggle-icon d-xl-none ms-auto">
-        <i class="bi bi-search"></i>
-    </div>
-    <form class="searchbar d-none d-xl-flex ms-auto">
-        <div class="position-absolute top-50 translate-middle-y search-icon ms-3"><i class="bi bi-search"></i></div>
-        <input class="form-control" type="text" placeholder="Type here to search">
-        <div class="position-absolute top-50 translate-middle-y d-block d-xl-none search-close-icon"><i
-            class="bi bi-x-lg"></i></div>
-    </form>
-    <div class="top-navbar-right ms-3">
+    <div class="top-navbar-right ms-auto">
         <ul class="navbar-nav align-items-center">
         <li class="nav-item dropdown dropdown-large d-none d-sm-block">
             <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" data-bs-toggle="dropdown">
