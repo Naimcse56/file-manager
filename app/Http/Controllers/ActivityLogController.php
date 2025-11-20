16a6ed11b0d802e356361483f6bb00d01b56efa4
unknown <naimcse56@gmail.com>
2025-11-20 17:55:22 +0600

    activity logs

--- app/Http/Controllers/ActivityLogController.php
+++ app/Http/Controllers/ActivityLogController.php
@@ -4,13 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Models\ActivityLog;
 use Illuminate\Http\Request;
+use DataTables;
 
 class ActivityLogController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        // Admin only
-        $logs = ActivityLog::with('user')->latest()->get(); 
-        return view('activity_logs.index', compact('logs'));
+        if ($request->ajax()) {
+            $data = ActivityLog::with('user')->latest();
+
+            return Datatables::of($data)
+                ->addIndexColumn()
+                ->editColumn('action', function ($row) {
+                    return ucfirst($row->action);
+                })
+                ->editColumn('target_type', function ($row) {
+                    return class_basename($row->target_type).' #'.$row->target_id ?? '-';
+                })
+                ->addColumn('created_at', function ($row) {
+                    return $row->created_at->format('d-M-Y H:i');
+                })
+                ->rawColumns(['avatar','action'])
+                ->make(true);
+        }
+        return view('activity_logs.index');
     }
 }
--- resources/views/activity_logs/index.blade.php
+++ resources/views/activity_logs/index.blade.php
@@ -1,36 +1,64 @@
 @extends('layouts.admin_app')
 
 @section('content')
-<div class="container mt-4">
-    <h3>Activity Logs</h3>
-
-    <table class="table table-bordered table-striped">
-        <thead class="table-dark">
-            <tr>
-                <th>#</th>
-                <th>User</th>
-                <th>Action</th>
-                <th>Target</th>
-                <th>Description</th>
-                <th>Date & Time</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse($logs as $log)
-            <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ $log->user?->name ?? 'System' }}</td>
-                <td>{{ ucfirst($log->action) }}</td>
-                <td>{{ class_basename($log->target_type) }} #{{ $log->target_id ?? '-' }}</td>
-                <td>{{ $log->description }}</td>
-                <td>{{ $log->created_at->format('d-M-Y H:i') }}</td>
-            </tr>
-            @empty
-            <tr>
-                <td colspan="6" class="text-center">No activity found.</td>
-            </tr>
-            @endforelse
-        </tbody>
-    </table>
+<div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+    <div class="breadcrumb-title pe-3">Activity Logs</div>
+    <div class="ps-3">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb mb-0 p-0">
+                <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
+                </li>
+                <li class="breadcrumb-item active" aria-current="page">Activity Logs List</li>
+            </ol>
+        </nav>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table id="data-table" class="table table-striped table-bordered" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th width="5%">#</th>
+								<th>User</th>
+                                <th>Action</th>
+                                <th>Target</th>
+                                <th>Description</th>
+                                <th>Date & Time</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
+@push('scripts')
+    <script type="text/javascript">
+        $(function () { 
+            "use strict";
+            var table = $('#data-table').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: "{{ route('activity-log.index') }}",
+                pageLength: 50,
+                columns: [
+                    {data: 'DT_RowIndex', name: 'DT_RowIndex', orderable: false, searchable: false},
+                    {data: 'user.name', name: 'user.name'},
+                    {data: 'action', name: 'action'},
+                    {data: 'target_type', name: 'target_type'},
+                    {data: 'description', name: 'description'},
+                    {data: 'created_at', name: 'created_at'},
+                ],
+            });
+            $.fn.dataTable.ext.errMode = () => alert('Error while loading the table data. Please refresh');
+                
+        });
+    </script>
+@endpush
