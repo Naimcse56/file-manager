2e26e11ddcfa8f480c2cc62cfb8de7ee03a67fd2
unknown <naimcse56@gmail.com>
2025-11-20 14:40:01 +0600

    controller and side menu

--- resources/views/layouts/partials/admin_sidebar.blade.php
+++ resources/views/layouts/partials/admin_sidebar.blade.php
@@ -1,37 +1,96 @@
 <aside class="sidebar-wrapper" data-simplebar="true">
    <div class="sidebar-header">
       <div>
-         <img src="{{asset('assets/images/logo.jpg')}}" class="logo-icon" alt="logo icon">
+         <img src="{{ asset('assets/images/logo.jpg') }}" class="logo-icon" alt="logo icon">
       </div>
       <div>
-         <h4 class="logo-text">{{env("APP_NAME")}}</h4>
+         <h4 class="logo-text">{{ env('APP_NAME') }}</h4>
       </div>
-      <div class="toggle-icon ms-auto"><i class="bi bi-chevron-double-left"></i>
-      </div>
+      <div class="toggle-icon ms-auto"><i class="bi bi-chevron-double-left"></i></div>
    </div>
+
    <!--navigation-->
    <ul class="metismenu" id="menu">
+
+      {{-- USER MANAGEMENT / SYSTEM SETTING --}}
       @canany(['role-list', 'user-list'])
-         <li>
-            <a href="javascript:;" class="has-arrow">
-               <div class="parent-icon"><i class="bi bi-grid"></i>
-               </div>
-               <div class="menu-title"> User Management/System Setting</div>
-            </a>
-            <ul>
-               @can(['user-list'])
-               <li class="@if (Route::is('user.*')) mm-active @endif">
-                  <a href="{{route('user.index')}}"><i class="bi bi-arrow-right-short"></i>User Information</a>
-               </li>
-               @endcan
-               @canany(['role-list','role-permission'])
-               <li class="@if (Route::is('user-management.*')) mm-active @endif">
-                  <a href="{{route('user-management.role-index')}}"><i class="bi bi-arrow-right-short"></i>Role Permission Assign</a>
-               </li>
-               @endcanany
-            </ul>
-         </li>
-      @endcan
+      <li>
+         <a href="javascript:;" class="has-arrow">
+            <div class="parent-icon"><i class="bi bi-grid"></i></div>
+            <div class="menu-title">User Management</div>
+         </a>
+         <ul>
+            @can('user-list')
+            <li class="@if (Route::is('user.*')) mm-active @endif">
+               <a href="{{ route('user.index') }}">
+                  <i class="bi bi-arrow-right-short"></i>User Information
+               </a>
+            </li>
+            @endcan
+
+            @canany(['role-list','role-permission'])
+            <li class="@if (Route::is('user-management.*')) mm-active @endif">
+               <a href="{{ route('user-management.role-index') }}">
+                  <i class="bi bi-arrow-right-short"></i>Role Permission Assign
+               </a>
+            </li>
+            @endcanany
+         </ul>
+      </li>
+      @endcanany
+
+
+      {{-- FILE MANAGER --}}
+      <li class="@if (Route::is('file-manager.*') || Route::is('folder.*')) mm-active @endif">
+         <a href="javascript:;" class="has-arrow">
+            <div class="parent-icon"><i class="bi bi-folder2-open"></i></div>
+            <div class="menu-title">File Manager</div>
+         </a>
+         <ul>
+            <li class="@if (Route::is('file-manager.index')) mm-active @endif">
+               <a href="{{ route('file-manager.index') }}">
+                  <i class="bi bi-arrow-right-short"></i>All Files
+               </a>
+            </li>
+
+            <li class="@if (Route::is('folder.index')) mm-active @endif">
+               <a href="{{ route('folder.index') }}">
+                  <i class="bi bi-arrow-right-short"></i>Folders
+               </a>
+            </li>
+
+            <li class="@if (Route::is('file-manager.upload')) mm-active @endif">
+               <a href="{{ route('file-manager.upload') }}">
+                  <i class="bi bi-arrow-right-short"></i>Upload Files
+               </a>
+            </li>
+
+            <li class="@if (Route::is('file-manager.trash')) mm-active @endif">
+               <a href="{{ route('file-manager.trash') }}">
+                  <i class="bi bi-arrow-right-short"></i>Recycle Bin
+               </a>
+            </li>
+         </ul>
+      </li>
+
+
+      {{-- SHARED FILE LINKS --}}
+      <li class="@if (Route::is('share-links.*')) mm-active @endif">
+         <a href="{{ route('share-links.index') }}">
+            <div class="parent-icon"><i class="bi bi-share"></i></div>
+            <div class="menu-title">Shared Files</div>
+         </a>
+      </li>
+
+
+      {{-- ACTIVITY LOG (Admin Only) --}}
+      <li class="@if (Route::is('activity-log.*')) mm-active @endif">
+         <a href="{{ route('activity-log.index') }}">
+            <div class="parent-icon"><i class="bi bi-clipboard-data"></i></div>
+            <div class="menu-title">Activity Logs</div>
+         </a>
+      </li>
+
    </ul>
    <!--end navigation-->
 </aside>
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\CityController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\FileManagerController;
+use App\Http\Controllers\FolderController;
+use App\Http\Controllers\ShareLinkController;
+use App\Http\Controllers\ActivityLogController;
 
 Auth::routes();
 
@@ -15,7 +18,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::post('/mututal-assesments/update-change-password', [HomeController::class, 'update_change_password'])->name('update_change_password');
 });
 
-Route::middleware(['auth', 'verified', 'check_password_changed'])->prefix('/mututal-assesments')->group(function () {
+Route::middleware(['auth', 'verified', 'check_password_changed'])->prefix('/file-manager')->group(function () {
     Route::controller(UserController::class)->prefix('/users')->group(function () {
         Route::get('/index', 'index')->name('user.index');
         Route::post('/store', 'store')->name('user.store');
@@ -23,30 +26,75 @@ Route::middleware(['auth', 'verified', 'check_password_changed'])->prefix('/mutu
         Route::post('/update/{id}', 'update')->name('user.update');
         Route::post('/delete', 'destroy')->name('user.delete');
         Route::get('/select-list-ajax', 'list_for_select_ajax')->name('user.list_for_select_ajax');
-        Route::get('/select-list-ajax-based-on-classification', 'list_for_select_ajax_based_on_classification')->name('user.list_for_select_ajax_based_on_classification');
-        Route::get('/select-list-ajax-cp-based-on-syndicate', 'list_for_select_ajax_cp_based_on_syndicate')->name('user.list_for_select_ajax_cp_based_on_syndicate');
 
     });
-    Route::controller(CityController::class)->prefix('/cities')->group(function () {
-        Route::get('/index', 'index')->name('cities.index');
-        Route::post('/store', 'store')->name('cities.store');
-        Route::get('/edit/{id}', 'edit')->name('cities.edit');
-        Route::post('/update/{id}', 'update')->name('cities.update');
-        Route::get('/select-list-ajax', 'list_for_select_ajax')->name('cities.list_for_select_ajax');
-        Route::get('/select-list-ajax-state', 'list_for_select_ajax_state')->name('state.list_for_select_ajax');
-        Route::get('/select-list-ajax-country', 'list_for_select_ajax_country')->name('country.list_for_select_ajax');
-
+    /*
+    |--------------------------------------------------------------------------
+    | FILE MANAGER ROUTES
+    |--------------------------------------------------------------------------
+    */
+    Route::controller(FileManagerController::class)->prefix('/file-manager')->group(function () {
+        Route::get('/index', 'index')->name('file-manager.index');
+        Route::get('/upload', 'uploadPage')->name('file-manager.upload');
+        Route::post('/store', 'store')->name('file-manager.store');
+        Route::get('/download/{file}', 'download')->name('file-manager.download');
+        Route::post('/restore/{id}', 'restore')->name('file-manager.restore');
+        Route::get('/trash', 'trash')->name('file-manager.trash');
+        Route::post('/delete/{file}', 'destroy')->name('file-manager.delete');
     });
-});
-Route::controller(RoleController::class)->prefix('user-management')->middleware(['auth', 'check_password_changed'])
-    ->group( function($route){
-        $route->get('role-assign-to-users', 'users')->name('user-management.user-index');
-        $route->get('role-index', 'index')->name('user-management.role-index');
-        $route->get('role-edit/{id}', 'editRole')->name('user-management.role-edit');
-        $route->post('role-update/{id}', 'updateRole')->name('user-management.role-update');
-        $route->post('role-assign', 'role_assign')->name('user-management.role-assign');
-        $route->post('role-store', 'store')->name('user-management.role-store');
 
-        $route->get('permission-index/{id}', 'permission_index')->name('user-management.permission-index');
-        $route->post('permission-store', 'permission_store')->name('user-management.permission-store');
+
+    /*
+    |--------------------------------------------------------------------------
+    | FOLDER ROUTES
+    |--------------------------------------------------------------------------
+    */
+    Route::controller(FolderController::class)->prefix('/folders')->group(function () {
+        Route::get('/index', 'index')->name('folder.index');
+        Route::post('/store', 'store')->name('folder.store');
+        Route::get('/show/{folder}', 'show')->name('folder.show');
+        Route::post('/update/{folder}', 'update')->name('folder.update');
+        Route::post('/delete/{folder}', 'destroy')->name('folder.delete');
+    });
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | SHARE LINK ROUTES
+    |--------------------------------------------------------------------------
+    */
+    Route::controller(ShareLinkController::class)->prefix('/share-links')->group(function () {
+        Route::get('/index', 'index')->name('share-links.index');
+        Route::post('/create/{file}', 'store')->name('share-links.store');
+        Route::get('/view/{token}', 'view')->name('share-links.view');
+        Route::post('/delete/{share}', 'destroy')->name('share-links.delete');
+    });
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | ACTIVITY LOG ROUTES
+    |--------------------------------------------------------------------------
+    */
+    Route::controller(ActivityLogController::class)->prefix('/activity-logs')->group(function () {
+        Route::get('/index', 'index')->name('activity-log.index');
+    });
+
+    /*
+    |--------------------------------------------------------------------------
+    | ROLE PERMISSION ROUTES
+    |--------------------------------------------------------------------------
+    */
+    Route::controller(RoleController::class)->prefix('user-management')->middleware(['auth', 'check_password_changed'])
+        ->group( function($route){
+            $route->get('role-assign-to-users', 'users')->name('user-management.user-index');
+            $route->get('role-index', 'index')->name('user-management.role-index');
+            $route->get('role-edit/{id}', 'editRole')->name('user-management.role-edit');
+            $route->post('role-update/{id}', 'updateRole')->name('user-management.role-update');
+            $route->post('role-assign', 'role_assign')->name('user-management.role-assign');
+            $route->post('role-store', 'store')->name('user-management.role-store');
+
+            $route->get('permission-index/{id}', 'permission_index')->name('user-management.permission-index');
+            $route->post('permission-store', 'permission_store')->name('user-management.permission-store');
+    });
 });
